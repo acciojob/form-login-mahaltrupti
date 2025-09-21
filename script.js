3c90bda819ef6c15a0e09709e16bcf693fa92e44
mahaltrupti <truptimahal2004@gmail.com>
2025-09-22 00:42:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
 function getFormvalue() {
     //Write your code here
- const container = document.getElementById("container")
-            const form = document.querySelector('.ele-container')
-            const heading = document.getElementById("message")
-           
-            if(localStorage.getItem("email")){
-                container.classList.add("hide")
-                heading.classList.remove("hide")
-                heading.innerText = `Hello ${localStorage.getItem("name")}`
-            }
+  event.preventDefault();
 
-            form.addEventListener("submit",onSubmit)
+    // Get the values from the first name and last name input fields
+    const firstName = document.getElementById("fname").value;
+    const lastName = document.getElementById("lname").value;
 
-            function onSubmit(e){
-                e.preventDefault()
-                console.log(e,'event from from')
-
-                const name = document.getElementById("userName").value
-                const email = document.getElementById("userEmail").value
-                const password = document.getElementById("userPassword").value
-                // Store name of user
-                localStorage.setItem("name",name)
-                // Store email of user
-                localStorage.setItem("email",email)
-                // Store password of user
-                localStorage.setItem("password",password)
-
-            }
+    // Concatenate the names and display the full name in an alert
+    alert(`${firstName} ${lastName}`);
 }
